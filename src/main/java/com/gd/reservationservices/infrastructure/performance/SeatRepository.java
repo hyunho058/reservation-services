5efdb72ca9a357184ccd1dfa727655a5659d2579
Hyunho Kim <hyunho058@naver.com>
2023-07-09 12:09:49 +0900

    비관적 lock 적용 (#6)

--- src/main/java/com/gd/reservationservices/infrastructure/performance/SeatRepository.java
+++ src/main/java/com/gd/reservationservices/infrastructure/performance/SeatRepository.java
@@ -1,13 +1,16 @@
 package com.gd.reservationservices.infrastructure.performance;
 
 import com.gd.reservationservices.domain.performance.Seat;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
     Optional<Seat> findByPerformanceIdAndLocationAndNumber(Long performanceId,
                                                            String location,
                                                            Integer number);
