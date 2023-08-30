59f60b38475aea954ca7e160fd52b50950e24c88
hyunho058 <hyunho058@naver.com>
2023-08-30 16:19:24 +0900

    DB 분산을 위한 DataSource Read, Write 분기 처리

---
+++ src/main/java/com/reservation/common/config/DataSourceConfig.java
@@ -0,0 +1,54 @@
+package com.reservation.common.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean()
+    @ConfigurationProperties(prefix = "spring.datasource.master.hikari")
+    public DataSource masterDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean()
+    @ConfigurationProperties(prefix = "spring.datasource.slave.hikari")
+    public DataSource slaveDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Primary
+    @Bean
+    public DataSource dataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+
+    @Bean
+    public DataSource routingDataSource(
+            @Qualifier("masterDataSource") DataSource masterDataSource,
+            @Qualifier("slaveDataSource") DataSource slaveDataSource)
+    {
+        RoutingDataSource routingDataSourceImpl = new RoutingDataSource();
+        Map<Object, Object> targetDataSource = new HashMap<>();
+        targetDataSource.put("master", masterDataSource);
+        targetDataSource.put("slave", slaveDataSource);
+        routingDataSourceImpl.setTargetDataSources(targetDataSource);
+        routingDataSourceImpl.setDefaultTargetDataSource(masterDataSource);
+        return routingDataSourceImpl;
+    }
+}
