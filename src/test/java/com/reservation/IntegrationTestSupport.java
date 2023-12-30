0f7b0c3d51a81c30ff4b12925551292b536d4420
hyunho058 <hyunho058@naver.com>
2023-12-30 20:32:55 +0900

    redis Testcontainers 적용

--- src/test/java/com/reservation/IntegrationTestSupport.java
+++ src/test/java/com/reservation/IntegrationTestSupport.java
@@ -1,8 +1,11 @@
 package com.reservation;
 
+import com.reservation.config.TestContainerConfig;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+@ExtendWith(TestContainerConfig.class)
 @Transactional
 @SpringBootTest
 public class IntegrationTestSupport {
